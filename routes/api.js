b85e7643a906455db4ace9c4a242248886d9464b
Harsh Sinha <hs1294@nyu.edu>
2016-06-30 07:45:30 -0400

    Add post and verrified that it works

--- routes/api.js
+++ routes/api.js
@@ -16,7 +16,7 @@ router.use(function(req, res, next) {
 router.get('/:resource', function(req,res, next) {
     var resource = req.params.resource
 
-    ColorController.get(null, function(err, docs) {
+    ColorController.get(req.query, function(err, docs) {
         if (err) {
             logger.error("ColorController.get failed", err)
             res.json({
@@ -29,9 +29,34 @@ router.get('/:resource', function(req,res, next) {
                 output: docs
             })
         }
+
+        return
     })
+
+    return
 })
 
-// router.post()
+router.post('/:resource', function(req, res, next) {
+    var resource = req.params.resource
+
+    ColorController.create(req.body, function(err, doc) {
+        if (err) {
+            logger.error("ColorController.create failed", err)
+            res.json({
+                code: CONSTANTS.RETURN_CODES.ERROR,
+                message: err
+            })
+        } else {
+            res.json({
+                code: CONSTANTS.RETURN_CODES.SUCCESS,
+                output: doc
+            })
+        }
+
+        return
+    })
+
+    return
+})
 
 module.exports = router
